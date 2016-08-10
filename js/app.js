89fc8f5a940d146f3ce511fcecee3ecd41271628
Alex Wilkerson <alex@uncorrected.com>
2016-08-10 05:15:44 -0500

    i think i'm finished

--- js/app.js
+++ js/app.js
@@ -1,32 +1,55 @@
 $(document).ready(function(){
 
-  var screenText = "0";
-  var previousText = "0";
-  var operation = "";
-  updateScreen();
+  const ADD = 1;
+  const SUBTRACT = 2;
+  const MULTIPLY = 3;
+  const DIVIDE = 4;
+  const EQUALED = 5;
+  const NONE = 0;
+
+  var workingNum = "0";
+  var previousNum = "0";
+  var operation = NONE;
 
   $("#btn-c").click(function() {
-    screenText = "0";
-    previousText = "0";
-    updateScreen();
+    workingNum = "0";
+    previousNum = "0";
+    operation = NONE;
+    updateDisplay(workingNum);
+    updateOperation("");
   });
 
-  $("#btn-ce").click(function() {
-    screenText = previousText;
-    updateScreen();
+  $("#btn-invert").click(function() {
+    if (operation === EQUALED) {
+      workingNum = previousNum;
+    }
+    if (+workingNum == 0) {
+      if (workingNum[0] === "-") {
+        workingNum = workingNum.slice(1, workingNum.length);
+      } else {
+        workingNum = "-" + workingNum;
+      }
+    } else {
+      workingNum = (+workingNum * -1).toString();
+    }
+    updateDisplay(workingNum);
+    if (operation === EQUALED) {
+      previousNum = workingNum;
+      workingNum = "0";
+    }
   });
 
   $("#btn-point").click(function() {
-    if (screenText.valueOf() == 0 || operation === "equaled") {
-      if (operation === "equaled") {
-        operation = "";
-        previousText = "0";
-      }
-      screenText = "0.";
-    } else if (screenText.indexOf(".") === -1) {
-      screenText += ".";
+    if (+workingNum == 0) {
+      workingNum = "0.";
+      updateDisplay(workingNum);
+    } else if (workingNum.indexOf(".") === -1) {
+      workingNum += ".";
+      updateDisplay(workingNum);
     }
-    updateScreen();
+    if (operation === EQUALED) {
+      operation = NONE;
+    }
   });
 
   $("#btn-0").click(function() {
@@ -70,73 +93,114 @@ $(document).ready(function(){
   });
 
   $("#btn-plus").click(function() {
-    previousText = screenText;
-    screenText = "0";
-    updateScreen();
-    operation = "add";
+    if (operation) {
+      previousNum = calculate(previousNum, workingNum, operation);
+      updateDisplay(previousNum);
+    } else {
+      previousNum = workingNum;
+      updateDisplay(workingNum);
+    }
+    workingNum = "0";
+    operation = ADD;
+    updateOperation("+");
   });
 
   $("#btn-subtract").click(function() {
-    previousText = screenText;
-    screenText = "0";
-    updateScreen();
-    operation = "subtract";
+    if (operation) {
+      previousNum = calculate(previousNum, workingNum, operation);
+      updateDisplay(previousNum);
+    } else {
+      previousNum = workingNum;
+      updateDisplay(workingNum);
+    }
+    workingNum = "0";
+    operation = SUBTRACT;
+    updateOperation("-");
   });
 
   $("#btn-multiply").click(function() {
-    previousText = screenText;
-    screenText = "0";
-    updateScreen();
-    operation = "multiply";
+    if (operation) {
+      previousNum = calculate(previousNum, workingNum, operation);
+      updateDisplay(previousNum);
+    } else {
+      previousNum = workingNum;
+      updateDisplay(workingNum);
+    }
+    workingNum = "0";
+    operation = MULTIPLY;
+    updateOperation("*");
   });
   
   $("#btn-divide").click(function() {
-    previousText = screenText;
-    screenText = "0";
-    updateScreen();
-    operation = "divide";
+    if (operation) {
+      previousNum = calculate(previousNum, workingNum, operation);
+      updateDisplay(previousNum);
+    } else {
+      previousNum = workingNum;
+      updateDisplay(workingNum);
+    }
+    workingNum = "0";
+    operation = DIVIDE;
+    updateOperation("÷");
   });
 
   $("#btn-equals").click(function() {
-    switch(operation) {
-      case "add": 
-        screenText = (+screenText + +previousText).toString();
-        operation = "equaled";
-        updateScreen();
-        break;
-      case "subtract":
-        screenText = (+previousText - +screenText).toString();
-        operation = "equaled";
-        updateScreen();
-        break;
-      case "multiply": 
-        screenText = (+previousText * +screenText).toString();
-        operation = "equaled";
-        updateScreen();
-        break;
-      case "divide": 
-        screenText = (+previousText / +screenText).toString();
-        operation = "equaled";
-        updateScreen();
-        break;
+    if (operation && previousNum) {
+      workingNum = calculate(previousNum, workingNum, operation);
+      previousNum = workingNum;
+      updateDisplay(workingNum);
+      workingNum = "0";
+      operation = EQUALED;
+    } else {
+      operation = EQUALED;
     }
+    updateOperation("");
   });
 
-  function updateScreen(){
-    $("#screenText").text(screenText);
+  function updateOperation(op) {
+    $("#operation").text(op);
+  }
+
+  function updateDisplay(num){
+    $("#screenText").text(num);
   }
 
   function inputNum(num) {
-    if (screenText === "0" || operation === "equaled") {
-      if (operation === "equaled") {
-        operation = "";
-        previousText = "0";
-      }
-      screenText = num;
-    } else if (screenText.length < 9){
-      screenText += num;
+    if (!operation) {
+      updateOperation("");
     }
-    updateScreen();
+    if (workingNum === "0") {
+      workingNum = num;
+    } else if (workingNum.length < 9){
+      workingNum += num;
+    }
+    updateDisplay(workingNum);
+    if (operation === EQUALED) {
+      operation = NONE;
+    }
+  }
+
+  function calculate(num1, num2, op) {
+    switch (op) {
+      case ADD:
+        return (+num1 + +num2).toString();
+        break;
+      case SUBTRACT:
+        return (+num1 - +num2).toString();
+        break;
+      case MULTIPLY:
+        return (+num1 * +num2).toString();
+        break;
+      case DIVIDE:
+        if (+num2 == 0) {
+          return ":(";
+        }
+        return (+num1 / +num2).toString();
+        break;
+      default:
+        return num1;
+        break;
+    }
   }
 
 });
